34cdf2f129a3770ecca82c6e2f062e67b670a103
starrett67 <starrett67@gmail.com>
2017-09-18 13:32:55 -0400

    Dont create dir on upload

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 sudo apt-get update
 sudo apt-get install curl -y
 ls
-find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://ftp.epizy.com/htdocs/{} \;
+find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS -T {} ftp://ftp.epizy.com/htdocs/{} \;
 # wput dist/ ftp://$FTP_USER:$FTP_PASS@ftp.epizy.com/htdocs/
