92ddbe3481c65a329a020e2b9f06dd5a0d09ff04
starrett67 <starrett67@gmail.com>
2017-09-18 13:28:56 -0400

    fix ftp url

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,5 @@
 sudo apt-get update
 sudo apt-get install curl -y
 ls
-find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://192.168.1.158/htdocs/{} \;
+find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS --ftp-create-dirs -T {} ftp://ftp.epizy.com/htdocs/{} \;
+# wput dist/ ftp://$FTP_USER:$FTP_PASS@ftp.epizy.com/htdocs/
