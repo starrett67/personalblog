7f8bd16b739e867dd076ebc4f6bf003d29901cde
starrett67 <starrett67@gmail.com>
2017-09-18 13:43:55 -0400

    debug deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get install curl -y
 ls
-find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS -T {} ftp://ftp.epizy.com/htdocs/{} \;
+find dist/ -type f
+find dist/ -type f -exec curl -u $FTP_USER:$FTP_PASS -T {} ftp://ftp.epizy.com/htdocs/ \;
 # wput dist/ ftp://$FTP_USER:$FTP_PASS@ftp.epizy.com/htdocs/
