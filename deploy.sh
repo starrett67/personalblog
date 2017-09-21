8b256f2ff59ca07f59c4cfbd0874ef34269092fa
starrett67 <starrett67@gmail.com>
2017-09-21 14:25:24 -0400

    Fix curl commands

--- deploy.sh
+++ deploy.sh
@@ -4,10 +4,10 @@ sudo apt-get update
 sudo apt-get install curl -y
 
 #first clean up ftp directory
-for f in $(curl -u $FTP_USER:$FTP_PASS ftp://ftp.epizy.com htdocs/); do
-
+for f in $(curl ftp://$FTP_USER:$FTP_PASS@ftp.epizy.com htdocs/); do
+  echo "Deleting: $f"
   # Delete each file individually
-  curl -u $FTP_USER:$FTP_PASS ftp://ftp.epizy.com -Q "DELE htdocs/$f"
+  curl ftp://$FTP_USER:$FTP_PASS@ftp.epizy.com -Q "DELE htdocs/$f"
 done
 
 cd dist/
